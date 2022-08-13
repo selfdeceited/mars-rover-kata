ab27a4266b514991e99e29df8bf0a3c64dc61ee7
aryzhikov <aryzhikov@dodopizza.com>
2022-08-14 00:01:03 +0400

    final test

--- src/rover.spec.ts
+++ src/rover.spec.ts
@@ -1,44 +1,89 @@
-import { Planet, Rover } from "./Rover";
+import { Planet, Rover, isObstaclesReport } from "./Rover";
 
 describe("when rover is placed", () => {
     it('should be able to move forward', () => {
-        const rover = new Rover({x: 0, y: 0, direction: 'north'});
+        const rover = new Rover({x: 0, y: 0, looking: 'north'})
+
         rover.receiveCommands(['⬆'])
+
         expect(rover.isAt).toEqual({x: 0, y: 1})
     })
 
     it('should be able to move backward', () => {
-        const rover = new Rover({x: 0, y: 0, direction: 'north'});
+        const rover = new Rover({x: 0, y: 0, looking: 'north'})
+
         rover.receiveCommands(['⬇', '⬇'])
+
         expect(rover.isAt).toEqual({x: 0, y: -2})
     })
 
     it('should be able to move forward and backward', () => {
-        const rover = new Rover({x: 5, y: 5, direction: 'south'});
+        const rover = new Rover({x: 5, y: 5, looking: 'south'})
+
         rover.receiveCommands(['⬆', '⬆', '⬇', '⬆'])
+
         expect(rover.isAt).toEqual({x: 5, y: 3})
     })
 
 
     it('should be able to change direction', () => {
-        const rover = new Rover({x: 0, y: 0, direction: 'north'});
+        const rover = new Rover({x: 0, y: 0, looking: 'north'})
+
         rover.receiveCommands(['⬇', '⬅', '⬆', '➡', '⬆'])
+
         expect(rover.isAt).toEqual({x: -1, y: 0})
     })
 
     describe("on a planet", () => {
         it('should come back after reaching the planet end', () => {
             const mercury: Planet = { size: 10 }
-            const rover = new Rover({x: 0, y: 0, direction: 'north', at: mercury })
-            rover.receiveCommands(Array(11).fill('⬆'))
+            const rover = new Rover({x: 0, y: 0, looking: 'north', at: mercury })
+
+            rover.receiveCommands(Array(mercury.size + 1).fill('⬆'))
+
             expect(rover.isAt).toEqual({x: 0, y: 0})
         })
     
         it('should return positive result based on a planet size', () => {
             const mercury: Planet = { size: 10 }
-            const rover = new Rover({x: 0, y: 0, direction: 'north', at: mercury })
+            const rover = new Rover({x: 0, y: 0, looking: 'north', at: mercury })
+
             rover.receiveCommands(['⬇'])
-            expect(rover.isAt).toEqual({x: 0, y: 10})
+
+            expect(rover.isAt).toEqual({x: 0, y: mercury.size})
+        })
+
+        // todo: mb cover if [10, 10] to be the same as [3,3] since it's not a square...
+        it('should cover edge of the map from both sides', () => {
+            const mercury: Planet = { size: 10 }
+            const rover = new Rover({x: 0, y: 0, looking: 'north', at: mercury })
+            const moveForward = Array(mercury.size + 1).fill('⬆')
+
+            rover.receiveCommands(['➡', ...moveForward, '⬅', ...moveForward])
+
+            expect(rover.isAt).toEqual({x: 0, y: 0})
+        })
+
+        it('should report when obstacles happen', () => {
+            const expectedObstacle = {x: 2, y: 2}
+            const mercury: Planet = { size: 10, obstacles: [
+                {x: 0, y: 0},
+                expectedObstacle,
+                {x: 3, y:0},
+                {x: 4, y:0}]
+            }
+
+            const rover = new Rover({x: 1, y: 1, looking: 'east', at: mercury })
+
+            const result = rover.receiveCommands([
+                '⬆', '⬅',  // before an obstacle
+                '⬆'     ,  // oops
+                '⬅', '⬆'  // this should not matter as we already bumped into an obstacle
+            ])
+
+            expect(rover.isAt).toEqual({x: 2, y: 1})
+            expect(isObstaclesReport(result)).toBe(true)
+            expect((result as any).obstacle).toEqual(expectedObstacle)
         })
     })
 })
--- src/rover.ts
+++ src/rover.ts
@@ -2,9 +2,15 @@ import { Command, Coordinates } from "./types"
 import { Direction, directionMap, isDirectionCommand } from "./direction"
 import { isMovementCommand, movementCommandMap } from "./movement"
 
-export type Planet = { size: number }
+export type Planet = { size: number, obstacles?: Array<Coordinates> }
 
-export type RoverStartInput = Coordinates & { direction: Direction, at?: Planet }
+export type RoverStartInput = Coordinates & { looking: Direction, at?: Planet }
+
+type CommandsResult = 'success' | { obstacle: Coordinates }
+export function isObstaclesReport(report: CommandsResult): report is { obstacle: Coordinates } {
+    return report !== 'success' && !!(report as any).obstacle
+}
+
 
 export class Rover {
     #x: number
@@ -12,10 +18,10 @@ export class Rover {
     #direction: Direction
     #planet: Planet | undefined
 
-    constructor({ x, y, direction, at }: RoverStartInput) {
+    constructor({ x, y, looking, at }: RoverStartInput) {
         this.#x = x
         this.#y = y
-        this.#direction = direction
+        this.#direction = looking
         this.#planet = at
     }
 
@@ -23,10 +29,20 @@ export class Rover {
         return { x: this.#x, y: this.#y }
     }
 
-    receiveCommands(commands: Command[]) {
+    receiveCommands(commands: Command[]): CommandsResult {
+        let detectedObstacle: Coordinates
+
         commands.forEach(command => {
+            if (!!detectedObstacle)
+                return
+
             if (isMovementCommand(command)) {
                 const { x, y } = movementCommandMap[command]({ x: this.#x, y: this.#y}, this.#direction, this.#planet)
+
+                if (this.detectObstacles(x, y)) {
+                    detectedObstacle = {x, y}
+                    return
+                }
 
                 this.#x = x
                 this.#y = y
@@ -34,5 +50,14 @@ export class Rover {
                 this.#direction = directionMap[command](this.#direction)
             }
         })
+
+        return detectedObstacle ? {obstacle: detectedObstacle} : 'success'
+    }
+
+    detectObstacles(x: number, y: number): boolean {
+        if (!this.#planet || !this.#planet.obstacles)
+            return false
+
+        return this.#planet.obstacles.some(o => o.x === x && o.y === y)
     }
 }
